2d160d6cdb07cbede3cf9a789d294f3ef19b27db
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-17 23:05:04 -0600

    commit tree 723e7710b07bc4e8623d05cc0e6910771581b1f1

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :bird_not_found
 
   # GET /birds
   def index
@@ -22,10 +23,34 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update
+    bird = find_bird
+    bird.update!(bird_params)
+    render json: bird
+  end
+
+  def increment_likes 
+    bird = find_bird
+    if bird
+      bird.update!(likes: bird.likes + 1)
+      render json: Bird
+    else
+      bird_not_found
+    end
+  end
+
   private
 
+  def find_bird
+    Bird.find(params[:id])
+  end
+
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
+  end
+
+  def bird_not_found
+    render json: { errors: ['Bird not found'] }, status: :not_found
   end
 
 end
